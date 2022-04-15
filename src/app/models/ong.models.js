536e848e7ded6bf82516d8cb0e48afe1c05861b6
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2022-04-15 20:11:10 -0300

    adicionado novo campo senha para o banco de dados

--- src/app/models/ong.models.js
+++ src/app/models/ong.models.js
@@ -4,17 +4,19 @@ async function createOng(data) {
   const query = `insert into ong(
   nome, 
   email,
+  senha,
   cnpj,
   tel,
   imagem,
   descricao
 ) values(
-$1, $2, $3, $4, $5, $6
+$1, $2, $3, $4, $5, $6, $7
 ) returning *  `;
 
   const values = [
     data.nomeOng,
     data.email,
+    data.senha,
     data.cnpj,
     data.tel,
     "teste",
