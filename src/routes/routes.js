d5fe5f8f3acf85b135eb4993011630db9a3029ce
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2022-04-15 18:12:03 -0300

    rota post para cadastrar uma nova ong

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,7 @@
 import express from "express";
 
+import ongController from "../app/controller/ong.controller.js";
+
 export const router = express.Router();
 
-router.get("/", (req, res) => res.send("foi"));
+router.post("/createOng", ongController.createOng);
