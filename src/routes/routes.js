557a66e6f938d3280d4ace9aa5338f5dff6ddf00
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2022-04-18 16:13:32 -0300

    colocado multer como middleware na rota put, para trabalhar com a imagem

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,9 @@
 import express from "express";
 
 import ongController from "../app/controller/ong.controller.js";
+import multer from "../middlewares/multer.js";
 
 export const router = express.Router();
 
 router.post("/createOng", ongController.createOng);
-router.put("/editOng", ongController.editOng);
+router.put("/editOng", multer.single("file"), ongController.editOng);
