632934391c4c7273b9825a41baf3d43f45acaf68
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2022-04-18 14:56:39 -0300

    salvando as alterações no banco

--- src/app/models/ong.models.js
+++ src/app/models/ong.models.js
@@ -26,6 +26,35 @@ $1, $2, $3, $4, $5, $6, $7
   return db.query(query, values);
 }
 
+function editOng(data) {
+  const query = `
+  update ong set 
+    nome=$1, 
+    email=$2,
+    senha=$3,
+    cnpj=$4,
+    tel=$5,
+    imagem=$6,
+    descricao=$7
+  
+    where id = $8
+  `;
+
+  const values = [
+    data.nome,
+    data.email,
+    data.senha,
+    data.cnpj,
+    data.tel,
+    "teste",
+    data.descricao,
+    data.id,
+  ];
+
+  return db.query(query, values);
+}
+
 export default {
   createOng,
+  editOng,
 };
