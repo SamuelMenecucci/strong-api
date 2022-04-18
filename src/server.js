19065359ddf3f35e5590d706ee39fc56d59165bb
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2022-04-18 18:08:39 -0300

    configuração do servidor para servir imagens estaticas

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import { router } from "./routes/routes.js";
 
 const app = express();
 
+app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
