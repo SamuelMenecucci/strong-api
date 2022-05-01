24d227d0375fb724cd0302c102b26801c1c96ce3
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2022-05-01 16:16:57 -0300

    adicionado middleware na rota de criação para fazer validação de campos e checagem de existencia de ong

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,10 +3,11 @@ import express from "express";
 import ongController from "../app/controller/ong.controller.js";
 import vacancyController from "../app/controller/vacancy.controller.js";
 import multer from "../middlewares/multer.js";
+import ongValidator from "../validators/ong.validator.js";
 
 export const router = express.Router();
 
-router.post("/createOng", ongController.createOng);
+router.post("/createOng", ongValidator.validOng, ongController.createOng);
 router.put("/editOng", multer.array("file", 1), ongController.editOng);
 
 router.get("/getVacancies", vacancyController.getVacancies);
@@ -19,3 +20,7 @@ router.get("/ongVacancies/:id", vacancyController.getOngVacancies);
 router.put("/editVacancy", vacancyController.editVacancy);
 
 router.delete("/deleteVacancy/:id", vacancyController.deleteVacancy);
+
+router.use((err, req, res, next) => {
+  res.status(400).send(err.message);
+});
