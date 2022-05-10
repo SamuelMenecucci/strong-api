4226b60bc73de1e7e35521606989197d71bc1936
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2022-05-09 21:34:41 -0300

    alteração da configuração do cors

--- src/server.js
+++ src/server.js
@@ -4,10 +4,16 @@ import { router } from "./routes/routes.js";
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET,HEAD,PUT,POST,DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
 app.use(router);
 
 app.listen(5001, () => console.log("server is running"));
